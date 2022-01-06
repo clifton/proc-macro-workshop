ccd7812583b065e42ae6c9d393ab25534e91865b
Clifton King <cliftonk@gmail.com>
2022-01-06 08:30:31 -0600

    added target for cargo expand
    $ cargo expand --bin expandme

---
+++ builder/src/bin/expandme.rs
@@ -0,0 +1,25 @@
+// used for `cargo expand`
+#![allow(dead_code)]
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    #[builder(each = "arg")]
+    args: Vec<String>,
+    #[builder(each = "env")]
+    env: Vec<String>,
+    current_dir: Option<String>,
+}
+
+fn main() {
+    let command = Command::builder()
+        .executable("cargo".to_owned())
+        .arg("build".to_owned())
+        .arg("--release".to_owned())
+        .build()
+        .unwrap();
+
+    assert_eq!(command.executable, "cargo");
+    assert_eq!(command.args, vec!["build", "--release"]);
+}
